2cf2e08af52f27541c51dc097e7f1b8f4fec00f9
bcoe <bencoe@google.com>
2021-03-08 18:03:17 -0800

    build: experiment with new build files

--- owlbot.py
+++ owlbot.py
@@ -11,39 +11,18 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""This script is used to synthesize generated parts of this library."""
 
-import os
-import synthtool as s
 import synthtool.gcp as gcp
 import synthtool.languages.node as node
 import logging
 
-# Infer version from src directory:
-versions = []
-default_version = 'v1beta3'
-for directory in os.listdir('./src'):
-  if os.path.isdir(os.path.join('./src', directory)):
-    versions.append(directory)
-name = 'documentai'
-
-## Note: this API only supports regional endpoints and does not support default scopes.
-s.replace(f"src/*/document_*_service_client.ts",
-          "servicePath !== staticMembers.servicePath && ",
-          ""
-         )
-s.replace(f"src/*/document_*_service_client.ts",
-          "and we're connecting to a non-default endpoint, set scopes just in case",
-          "set scopes"
-         )
-s.replace(f"src/*/document_*_service_client.ts",
-          r"// Set the default scopes in auth client if needed(.|\n)*?}",
-          ""
-         )
-
 # Copy common templates
 common_templates = gcp.CommonTemplates()
-templates = common_templates.node_library(source_location='build/src', versions=versions, default_version=default_version)
+templates = common_templates.node_library(
+    versions=node.detect_versions()
+    default_version='v1beta3'
+)
 s.copy(templates, excludes=[])
 
+# Lint, compile protos, etc.:
 node.postprocess_gapic_library_hermetic()
