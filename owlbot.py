43c521727a0b1d24ecc67a37699385f4672aafc1
bcoe <bencoe@google.com>
2021-03-09 09:46:54 -0800

    build: address code review

--- owlbot.py
+++ owlbot.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import synthtool as s
+import synthtool
 import synthtool.gcp as gcp
 import synthtool.languages.node as node
 import logging
@@ -23,7 +23,21 @@ templates = common_templates.node_library(
     versions=node.detect_versions(),
     default_version='v1beta3'
 )
-s.copy(templates, excludes=[])
+synthtool.copy(templates, excludes=[])
+
+## Note: this API only supports regional endpoints and does not support default scopes.
+s.replace(f"src/*/document_*_service_client.ts",
+          "servicePath !== staticMembers.servicePath && ",
+          ""
+         )
+s.replace(f"src/*/document_*_service_client.ts",
+          "and we're connecting to a non-default endpoint, set scopes just in case",
+          "set scopes"
+         )
+s.replace(f"src/*/document_*_service_client.ts",
+          r"// Set the default scopes in auth client if needed(.|\n)*?}",
+          ""
+         )
 
 # Lint, compile protos, etc.:
 node.postprocess_gapic_library_hermetic()
