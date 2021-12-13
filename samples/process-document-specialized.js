64410189f8921f1dbe39d219c813df623ef2de34
Gal Zahavi <38544478+galz10@users.noreply.github.com>
2021-12-13 14:19:40 -0800

    changed if statement

--- samples/process-document-specialized.js
+++ samples/process-document-specialized.js
@@ -74,7 +74,8 @@ async function main(projectId, location, processorId, filePath) {
       const key = entity.type;
       // some other value formats in addition to text are availible
       // e.g. dates: `entity.normalizedValue.dateValue.year`
-      const textValue = entity.textAnchor.content !== null ? entity.textAnchor.content : '';
+      const textValue = 
+             'content' in entity.textAnchor ? entity.textAnchor.content : '';
       const conf = entity.confidence * 100;
       console.log(
         `* ${JSON.stringify(key)}: ${JSON.stringify(textValue)}(${conf.toFixed(
